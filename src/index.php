3edc987daaf97b351617c540613054d5b0730f3c
fireohiro <148302631+fireohiro@users.noreply.github.com>
2024-01-09 14:30:22 +0900

    色々追加

---
+++ src/index.php
@@ -0,0 +1,21 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1517450-final';
+    const USER = 'LAA1517450';
+    const PASS = 'Pass0423';
+
+    $connect = 'mysql:host='.SERVER.';dbname='.DBNAME.';charset=utf8';
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>トップページ</title>
+</head>
+<body>
+    <font size="7" color="#ff8c00" face="丸ゴシック">観光名所</font><hr>
+</body>
+</html>
